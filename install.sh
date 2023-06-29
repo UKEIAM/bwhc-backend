ec6968d5b47a1e3223f35d863d17f75e8109a580
Hannes Voß <han.voss@uke.de>
2023-06-29 13:37:42 +0200

    Add UKE configuration

---
+++ install.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+
+
+TARGET_DIR=""
+
+
+if [ "$#" -eq "0" ]; then
+
+  echo "ERROR: Enter a target directory for bwHC backend installation"
+ 
+  exit 1
+
+else 
+
+  TARGET_DIR="$1"
+
+fi
+
+
+if [ ! -d "$TARGET_DIR" ]; then
+  mkdir -p "$TARGET_DIR"
+fi
+
+
+BWHC_APP_DIR="bwhc-rest-api-gateway-1.0-SNAPSHOT"  # Value 'bwhc-rest-api-gateway-1.0-SNAPSHOT' in this script template
+                                   # is replaced by actual value upon packaging
+
+BWHC_ZIP="$BWHC_APP_DIR.zip"
+
+PRODUCTION_CONF="production.conf"
+
+SERVICE_SCRIPT="bwhc-backend-service"
+
+CONFIG="config"
+
+BWHC_CONNECTOR_CONFIG="bwhcConnectorConfig.xml"
+
+LOGBACK="logback.xml"
+
+
+
+
+cp $BWHC_ZIP "$TARGET_DIR/"
+
+#cp $SERVICE_SCRIPT "$TARGET_DIR/"
+if [ ! -f "$TARGET_DIR/$SERVICE_SCRIPT" ]; then
+
+  echo "Copying $SERVICE_SCRIPT ..."
+
+  cp $SERVICE_SCRIPT "$TARGET_DIR/"
+
+fi
+
+
+
+
+if [ ! -f "$TARGET_DIR/$CONFIG" ]; then
+
+  echo "Copying $CONFIG ..."
+
+  cp $CONFIG "$TARGET_DIR/"
+
+fi
+
+
+if [ ! -f "$TARGET_DIR/$PRODUCTION_CONF" ]; then
+
+  echo "Copying $PRODUCTION_CONF ..."
+
+  cp $PRODUCTION_CONF "$TARGET_DIR/"
+
+fi
+
+
+if [ ! -f "$TARGET_DIR/$BWHC_CONNECTOR_CONFIG" ]; then
+
+  echo "Copying $BWHC_CONNECTOR_CONFIG ..."
+
+  cp $BWHC_CONNECTOR_CONFIG "$TARGET_DIR/"
+
+fi
+
+
+if [ ! -f "$TARGET_DIR/$LOGBACK" ]; then
+  
+  echo "Copying $LOGBACK ..."
+
+  cp $LOGBACK "$TARGET_DIR/"
+
+fi
+
+
+
+cd "$TARGET_DIR"
+
+
+if [ -d "$BWHC_APP_DIR" ]; then
+
+  echo " Removing previous bwHC backend app"
+
+  rm -r $BWHC_APP_DIR
+
+fi
+
+unzip $BWHC_ZIP
+
